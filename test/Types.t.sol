432a07acd7656727339d955ca4e56b0e84febbb0
PurrProof <149718167+PurrProof@users.noreply.github.com>
2024-03-23 14:19:01 +0100

    add types tests

---
+++ test/Types.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import "forge-std/StdError.sol";
+
+contract TypesTest is Test {
+    function setUp() public {}
+
+    function test_Math() public pure {
+        assertEq(type(int8).min, int8(-128));
+    }
+
+    // The expression type(int).min / (-1) is the only case where division causes an overflow.
+    // In checked arithmetic mode, this will cause a failing assertion,
+    // while in wrapping mode, the value will be type(int).min.
+
+    function test_Overflow1() public {
+        vm.expectRevert(stdError.arithmeticError);
+        int8 res = type(int8).min / int8(-1);
+    }
+    function test_Overflow2() public {
+        vm.expectRevert(stdError.arithmeticError);
+        int8 res = type(int8).min * int8(-1);
+    }
+    function test_Unchecked() public pure {
+        assertEq(type(int8).min, -128);
+        unchecked {
+            assertEq(type(int8).min / int8(-1), -128);
+            assertEq(type(int8).min * int8(-1), -128);
+        }
+    }
+}
