c5faf71384bf272f154d367fe3d782f66a538e83
PurrProof <149718167+PurrProof@users.noreply.github.com>
2024-08-23 18:29:00 +0200

    add ERC 1967 basic test

--- src/proxy/Erc1967Proxy.sol
+++ src/proxy/Erc1967Proxy.sol
@@ -2,5 +2,12 @@
 pragma solidity ^0.8.26;
 
 import {ERC1967Proxy} from "@openzeppelin-contracts-5.0.2/proxy/ERC1967/ERC1967Proxy.sol";
+import {ERC1967Utils} from "@openzeppelin-contracts-5.0.2/proxy/ERC1967/ERC1967Utils.sol";
 
-contract MyErc1967Proxy {}
+contract MyErc1967Proxy is ERC1967Proxy {
+    constructor(address impl) ERC1967Proxy(impl, "") {}
+
+    function implementation() public view returns (address impl) {
+        return ERC1967Utils.getImplementation();
+    }
+}
--- test/proxy/Erc1967Proxy.t.sol
+++ test/proxy/Erc1967Proxy.t.sol
@@ -2,14 +2,26 @@
 pragma solidity ^0.8.26;
 
 import {Test} from "forge-std/Test.sol";
+import {Counter} from "../../src/Counter.sol";
 import {MyErc1967Proxy} from "../../src/proxy/Erc1967Proxy.sol";
-
-/* solhint-disable comprehensive-interface */
+import {ERC1967Utils} from "@openzeppelin-contracts-5.0.2/proxy/ERC1967/ERC1967Utils.sol";
 
 contract MyErc1967ProxyTest is Test {
     MyErc1967Proxy public proxy;
+    Counter public counter;
 
     function setUp() public {
-        proxy = new MyErc1967Proxy();
+        counter = new Counter();
+        proxy = new MyErc1967Proxy(address(counter));
+    }
+
+    function test_implementation() public {
+        bytes32 slot = bytes32(uint256(keccak256("eip1967.proxy.implementation")) - 1);
+
+        address storedImpl = address(uint160(uint256(vm.load(address(proxy), slot))));
+        emit log_address(storedImpl);
+
+        assertEq(storedImpl, proxy.implementation());
+        assertEq(storedImpl, address(counter));
     }
 }
